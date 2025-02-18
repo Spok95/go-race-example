40bec830222ace3092e50656a3c53374b513fcf3
Константин <kostya2211@yandex.ru>
2025-02-18 14:47:58 +0300

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"sync"
 	"testing"
-	"time"
 )
 
 func TestMaxInt(t *testing.T) {
@@ -17,19 +15,5 @@ func TestMaxInt(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
-	var wg sync.WaitGroup
-	numGoroutines := 100 // Увеличим количество горутин
-	wg.Add(numGoroutines)
-
-	for i := 0; i < numGoroutines; i++ {
-		go func() {
-			defer wg.Done()
-			for j := 0; j < 100; j++ { // Увеличим количество итераций
-				main()                       // Запускаем main() несколько раз
-				time.Sleep(time.Millisecond) // Небольшая задержка для усиления гонки
-			}
-		}()
-	}
-
-	wg.Wait()
+	main()
 }
